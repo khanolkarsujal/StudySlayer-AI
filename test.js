9a768a6c04862f5445f2971404160310883efe94
Khanolkar sujal <khanolkarsujal@gmail.com>
2026-03-22 12:22:49 +0530

    Override main branch with updated frontend environment setting

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-require('dotenv').config();
-const API_KEY = process.env.GEMINI_API_KEY;
+import fs from 'node:fs';
+const envFile = fs.readFileSync('./.env', 'utf8');
+const API_KEY = envFile.match(/GEMINI_API_KEY=(.*)/)[1].trim();
 fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
